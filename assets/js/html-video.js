44fd8980f2793d262a54658a1389743948884039
sricharankrishnan <sricharan.krishnan@gmail.com>
2020-02-14 18:34:18 +0530

    new ui build for the website

---
+++ assets/js/html-video.js
@@ -0,0 +1,170 @@
+/* this javascript file helps to dynamcically build a html video
+   background for a particular element. has specific css requirements to work */
+
+/* this function helps to build html video dynamically in the document 
+  when the user is in the view of the respective html section */
+function buildHtmlVideo(configObject) {
+  var _opts = Object.prototype.toString;
+  try {
+    if (_opts.call(configObject) !== "[object Object]") {
+      throw new Error(
+        "- requires the configObject argument to be of object type, please check."
+      );
+      return;
+    } else if (typeof configObject.webmVideo !== "string") {
+      throw new Error(
+        "- requires the WebmVideo Property to be of string type. Please check."
+      );
+      return;
+    } else if (typeof configObject.mp4Video !== "string") {
+      throw new Error(
+        "- requires the Mp4Video Property to be of string type. Please check."
+      );
+      return;
+    } else if (configObject.parentElement.get(0).nodeType !== 1) {
+      throw new Error(
+        "- requires the ParentElement property to be of Node Type 1. Please Check."
+      );
+      return;
+    } else if (typeof configObject.playInMobile !== "boolean") {
+      throw new Error(
+        " - requires property/field playInMobile to be of Boolean Type"
+      );
+      return;
+    } else if (typeof configObject.playInTablet !== "boolean") {
+      throw new Error(
+        " - requires property/field playInTablet to be of Boolean Type"
+      );
+      return;
+    } else if (typeof configObject.playInDesktop !== "boolean") {
+      throw new Error(
+        " - requires property/field playInDesktop to be of Boolean Type"
+      );
+      return;
+    } else if (typeof configObject.fallbackImage !== "string") {
+      throw new Error(
+        " - requires property/field fallbackImage to be of String Type."
+      );
+      return;
+    } else {
+      var pe = configObject.parentElement;
+
+      function checkIE() {
+        var undef,
+          v = 3,
+          div = document.createElement("div");
+        while (
+          ((div.innerHTML = "<!--[if gt IE " + ++v + "]><i></i><!endif-->"),
+          div.getElementsByTagName("i")[0])
+        );
+        return v > 4 ? v : undef;
+      }
+      var isIE = checkIE();
+      conditionsMiddleware();
+    }
+
+    /* makes sure that we have certain conditions that are ok */
+    function conditionsMiddleware() {
+      var mp4Status,
+        webmStatus,
+        formats = {
+          mp4: 'video/mp4; codecs="avc1.42E01E"',
+          webm: 'video/webm; codecs="vp8, vorbis"'
+        };
+
+      var v = document.createElement("video");
+      mp4Status = v.canPlayType(formats["mp4"]) === "probably" ? true : false;
+      webmStatus = v.canPlayType(formats["webm"]) === "probably" ? true : false;
+
+      if (
+        configObject.playInMobile === false &&
+        configObject.playInTablet === false &&
+        configObject.playInDesktop === false
+      ) {
+        var bgurl = configObject.fallbackImage;
+        buildHtmlVideo.buildBgPicture(pe, bgurl);
+      } else if (isIE === true) {
+        var bgurl = configObject.fallbackImage;
+        buildHtmlVideo.buildBgPicture(pe, bgurl);
+      } else if (mp4Status === false && webmStatus === false) {
+        var bgurl = configObject.fallbackImage;
+        buildHtmlVideo.buildBgPicture(pe, bgurl);
+      } else if (mp4Status === true) {
+        buildHtmlVideo.checkDeviceWidth(configObject, "mp4");
+      } else {
+        buildHtmlVideo.checkDeviceWidth(configObject, "webm");
+      }
+    }
+  } catch (thisErr) {
+    console.error("Build HTML5 Video Function");
+    console.error(thisErr);
+  }
+}
+/* templates the requires video element with the url and parent element */
+buildHtmlVideo.checkDeviceWidth = function(configObject, vt) {
+  var w = $(window),
+    vurl =
+      vt === "mp4"
+        ? configObject.mp4Video
+        : vt === "webm"
+        ? configObject.webmVideo
+        : false,
+    pe = configObject.parentElement;
+
+  if ($(w).innerWidth() <= 767 && configObject.playInMobile === true) {
+    buildHtmlVideo.templateElements(pe, vurl);
+  }
+  if ($(w).innerWidth() <= 767 && configObject.playInMobile === false) {
+    buildHtmlVideo.buildBgPicture(
+      configObject.parentElement,
+      configObject.fallbackImage
+    );
+  }
+
+  if (
+    $(w).innerWidth() >= 768 &&
+    $(w).innerWidth() <= 1199 &&
+    configObject.playInTablet === true
+  ) {
+    buildHtmlVideo.templateElements(pe, vurl);
+  }
+  if (
+    $(w).innerWidth() >= 768 &&
+    $(w).innerWidth() <= 1199 &&
+    configObject.playInTablet === false
+  ) {
+    buildHtmlVideo.buildBgPicture(
+      configObject.parentElement,
+      configObject.fallbackImage
+    );
+  }
+
+  if ($(w).innerWidth() >= 1200 && configObject.playInDesktop === true) {
+    buildHtmlVideo.templateElements(pe, vurl);
+  }
+  if ($(w).innerWidth() >= 1200 && configObject.playInDesktop === false) {
+    buildHtmlVideo.buildBgPicture(
+      configObject.parentElement,
+      configObject.fallbackImage
+    );
+  }
+};
+/* builds the required html5 video elements */
+buildHtmlVideo.templateElements = function(pe, vurl) {
+  var videoParent = $('<div class="videoParent"></div>'),
+    videoTag = $(
+      '<video style="opacity:0.98;" autoplay loop muted playsinline></video>'
+    );
+
+  videoTag.attr("src", vurl);
+  videoTag.appendTo(videoParent);
+  videoParent.appendTo(pe);
+  pe.find(".videoParent").removeClass("hiddenTransform");
+};
+/* templates the background image for the parent element */
+buildHtmlVideo.buildBgPicture = function(pe, bgurl) {
+  pe.css({
+    background: "url(" + bgurl + ") no-repeat center center",
+    "background-size": "cover"
+  });
+};
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,4 @@
-(function(w, d) {
-  var $websiteRoot = $("#outerBorder");
-
+(function($mainRoot, w, d) {
   function initDemoVideos() {
     var trig1 = $("#demo1"),
         trig2 = $("#demo2"),
@@ -59,11 +57,179 @@
       }
     });
   }
+  
+  /* checks the window scroll pos and then, will trigger the necessary callback fuction */
+  function checkWindowScrollPosition(thisParentElement, thisCallback) {
+    try {
+      if (!thisParentElement || !thisCallback) {
+        throw new Error(
+          "requires 2 mandatory arugments for functioning. Please check what has been passed"
+        );
+      } else if (typeof thisCallback !== "function") {
+        throw new Error(
+          "requires the argument 'thisCallback' to be of function type only"
+        );
+      } else {
+        $(window).on("scroll", function() {
+          var elementTopPos = thisParentElement.offset().top,
+            elementBotPos = elementTopPos + thisParentElement.outerHeight(),
+            viewportTopPos = $(window).scrollTop(),
+            viewportBotPos = viewportTopPos + $(window).height(),
+            hasDataLoaded = thisParentElement.attr("data-hasloaded");
+
+          if (
+            elementBotPos > viewportTopPos &&
+            elementTopPos < viewportBotPos &&
+            hasDataLoaded === "false"
+          ) {
+            thisCallback();
+          }
+        });
+      }
+    } catch (thisError) {
+      console.error("checkWindowScrollPosition() -> " + thisError);
+    }
+  }
+
+  /* helps to build the required elements for the jumbotron section */
+  function jumbotronInit() {
+    var pe = $mainRoot.find("#jumbotronSection");
+    var insertElement = pe.find(".customRow"); 
+    var configObject = {
+      webmVideo: pe.attr("data-webm"),
+      mp4Video: pe.attr("data-mp4"),
+      parentElement: insertElement,
+      playInMobile: true,
+      playInTablet: true,
+      playInDesktop: true,
+      fallbackImage: ""
+    };
+    buildHtmlVideo(configObject);
+  }
+
+  /* helps control the demo section for the page */
+  function demoSectionInit() {
+    var pe = $mainRoot.find("#demoSectionParent");
+    var cp = pe.find("#demoCarousel");
+    var cpConfig = {
+      loop: true,
+      rewind: true,
+      nav: false,
+      dots: true,
+      responsive: {
+        0: {
+          items: 1,
+          stagePadding: 20
+        },
+        768: {
+          items: 3,
+          stagePadding: 20
+        },
+        992: {
+          items: 3,
+          stagePadding: 20
+        },
+        1200: {
+          loop: false,
+          items: 5,
+          stagePadding: 20
+        }
+      }
+    };
+
+    cp.owlCarousel(cpConfig);
+
+    checkWindowScrollPosition(pe, function() {
+      pe.attr("data-hasloaded", "true");
+      demoSectionInit.loadCarouselImages.call(cp);
+      demoSectionInit.loadVideoPlayer.call(cp);
+    });
+  }
+  /* loads carousel images */
+  demoSectionInit.loadCarouselImages = function() {
+    var $this = this;
+    var items = $this.find(".carouselItem");
+    var len = items.length;
+    var counter = 0;
+
+    function startImageLoad() {
+      var thisitem = items.eq(counter);
+      var imgsrc = thisitem.attr("data-img");
+      var imgname = thisitem.attr("data-imgname");
+      var img = $('<img class="img-responsive center-block"/>');
+
+      img.attr({
+        "title": imgname,
+        "alt": imgname,
+        "src": imgsrc
+      })
+      .on("load", function() {
+        thisitem.removeClass("isLoading").find(".borderBoxContainer").append(img);
+        nextIteration();
+      })
+      .on("error", function() {
+        nextIteration();
+      });
+
+      function nextIteration() {
+        ++counter;
+        if(counter < len) {
+          setTimeout(startImageLoad, 60);
+        }
+      }
+    }
+    startImageLoad();
+  };
+  /* loads the youtube js overlay */
+  demoSectionInit.loadVideoPlayer = function() {
+    var $this = this;
+    var citems = $this.find(".carouselItem");
+    var len = citems.length;
+
+    for (var i = 0; i < len; ++i) {
+      citems.eq(i).attr("id", "item" + i);
+    }
+
+    citems.each(function(index, thisitem) {
+      var thisitem = $(thisitem);
+      var thisitemBuild = new YoutubeOverlayModule({
+        sourceUrl: thisitem.attr("data-videourl"),
+        triggerElement: "#" + thisitem.attr("id"),
+        progressCallback: function() {
+          console.log("Item activated");
+        }
+      });
+      thisitemBuild.activateDeployment();
+    });
+  }
+
+  /* footer section handler */
+  function footerSectionInit() {
+    var pe = $("#appFooterParent");
+
+    console.log(pe);
+    checkWindowScrollPosition(pe, function() {
+      pe.attr("data-hasloaded", "true");
+      var configObject = {
+        webmVideo: pe.attr("data-webm"),
+        mp4Video: pe.attr("data-mp4"),
+        parentElement: pe,
+        playInMobile: true,
+        playInTablet: true,
+        playInDesktop: true,
+        fallbackImage: ""
+      };
+      buildHtmlVideo(configObject);
+    });
+  }
 
   function centralController() {
-    initDemoVideos();
+    //initDemoVideos();
 		smoothScroll();
+    jumbotronInit();
+    demoSectionInit();
+    footerSectionInit();
   }
 
   $(d).ready(centralController);
-})(window, document);
+})($("#outerBorder"), window, document);
